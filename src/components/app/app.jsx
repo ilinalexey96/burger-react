8a627a44597089700780906c61c9b7104f068b7b
Ilin Alexey <ilin.alexey96@gmail.com>
2023-04-18 23:58:58 +0700

    add header

---
+++ src/components/app/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './app.module.css'
+import { AppHeader } from '../app-header/app-header.jsx';
+
+export default function App() {
+  return (
+    <AppHeader />
+  );
+}
+
