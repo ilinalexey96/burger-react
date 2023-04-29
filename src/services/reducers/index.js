e8cb16a63d67cd6e0442797c2f2d9da5f8ad92c4
Ilin Alexey <ilin.alexey96@gmail.com>
2023-04-29 14:54:29 +0700

    add rootReducer

---
+++ src/services/reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux";
+import { burgerIngredientsReducer } from "./burger-ingredients";
+import { ingredientDetailsReducer } from "./ingredient-details";
+import { burgerConstructorReducer } from "./burger-constructor";
+import { orderDetailsReducer } from "./order-details";
+import { scrollIngredientsReducer } from "./burger-ingredients-scroll";
+
+export const rootReducer = combineReducers({
+    burgerIngredients: burgerIngredientsReducer,
+    ingredientDetails: ingredientDetailsReducer,
+    burgerConstructor: burgerConstructorReducer,
+    orderDetails: orderDetailsReducer,
+    scrollIngredients: scrollIngredientsReducer
+})
