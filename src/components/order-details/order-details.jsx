42481c5ac64116163f6d950a47d1fa6781109292
Ilin Alexey <ilin.alexey96@gmail.com>
2023-05-01 16:46:54 +0700

    add order-registration

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,16 +1,20 @@
 import styles from './order-details.module.css';
 import done from '../../images/done.svg';
+import { useSelector } from 'react-redux';
 
-export default function OrderDetails() {
+export function OrderDetails() {
+
+    const orderNumber = useSelector(state => state.orderDetails.id)
+
     return (
         <>
-        <div className={`${styles.order_details} pl-25 pr-25`}>
-            <p className={`${styles.order_identifier} text text_type_digits-large mt-30 mb-8`}>034536</p>
-            <p className='text text_type_main_-medium mb-15'>Идентификатор заказа</p>
-            <img src={done} className={styles.done} alt='Заказ подтвержден'/>
-            <p className='text text_type_main-default mt-15 mb-2'>Ваш заказ начали готовить</p>
-            <p className='text text_type_main-default text_color_inactive mb-30'>Дождитесь готовности на орбитальной станции</p>
-        </div>
+            <div className={`${styles.order_details} pl-25 pr-25`}>
+                <p className={`${styles.order_identifier} text text_type_digits-large mt-30 mb-8`}>{orderNumber}</p>
+                <p className="text text_type_main-medium mb-15">идентификатор заказа</p>
+                <img src={done} className={styles.done} alt="Заказ подтвержден" />
+                <p className="text text_type_main-default mt-15 mb-2">Ваш заказ начали готовить</p>
+                <p className="text text_type_main-default text_color_inactive mb-30">Дождитесь готовности на орбитальной станции</p>
+            </div>
         </>
     )
 }
